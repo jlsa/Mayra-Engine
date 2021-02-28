9aca6a301a1c6de44d06e0687daf8a6fcf2d41fb
jlsa <jlsahoekstra@gmail.com>
2021-02-28 14:45:06 +0100

    added a way to scale the whole font render field

--- Mayra/Scenes/FreeTypeFontScene.cpp
+++ Mayra/Scenes/FreeTypeFontScene.cpp
@@ -1,17 +1,5 @@
 #include "FreeTypeFontScene.h"
 
-static void HelpMarker(const char* desc)
-{
-    ImGui::TextDisabled("(?)");
-    if (ImGui::IsItemHovered())
-    {
-        ImGui::BeginTooltip();
-        ImGui::PushTextWrapPos(ImGui::GetFontSize() * 35.0f);
-        ImGui::TextUnformatted(desc);
-        ImGui::PopTextWrapPos();
-        ImGui::EndTooltip();
-    }
-}
 struct InputTextCallback_UserData
 {
     std::string*            Str;
@@ -42,7 +30,7 @@ static int InputTextCallback(ImGuiInputTextCallbackData* data)
 namespace Mayra
 {
     FreeTypeFontScene::FreeTypeFontScene()
-      : outputString("test hallo"), m_ClearColor(glm::vec4(62.0f / 255.0f, 61.0f / 255.0f, 64.0f / 255.0f, 1.0f))
+      : outputString("> example.txt"), m_ClearColor(glm::vec4(62.0f / 255.0f, 61.0f / 255.0f, 64.0f / 255.0f, 1.0f))
     {
         m_Camera = new OrthographicCamera(-1.6f, 1.6f, -0.9f, 0.9f);
         m_Projection = m_Camera->GetProjectionMatrix();
@@ -77,15 +65,6 @@ namespace Mayra
     void FreeTypeFontScene::OnImGuiRender()
     {
         ImGui::Begin("TextRendering");
-        static char password[64] = "password123";
-
-        if (ImGui::InputText("password", password, IM_ARRAYSIZE(password), ImGuiInputTextFlags_Password))
-        {
-            outputString = std::string(password);
-        }
-        ImGui::SameLine(); HelpMarker("Display all characters as '*'.\nDisable clipboard cut and copy.\nDisable logging.\n");
-        ImGui::InputTextWithHint("password (w/ hint)", "<password>", password, IM_ARRAYSIZE(password), ImGuiInputTextFlags_Password);
-        ImGui::InputText("password (clear)", password, IM_ARRAYSIZE(password));
 
         if (outputString.empty())
             outputString.push_back(0);
@@ -94,7 +73,7 @@ namespace Mayra
 
         if (questString.empty())
             questString.push_back(0);
-        FreeTypeFontScene::InputTextMultiline("##Quest", &questString, ImVec2(-FLT_MIN, ImGui::GetTextLineHeight() * 5));
+        FreeTypeFontScene::InputTextMultiline("##Quest", &questString, ImVec2(-FLT_MIN, ImGui::GetTextLineHeight() * 15));
         ImGui::Text("Data: %hhd\nSize:%lu\nCapacity: %lu", questString.c_str()[0], questString.size(), questString.capacity());
 
         ImGui::End();
@@ -151,6 +130,9 @@ namespace Mayra
         glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(1280), 0.0f, static_cast<float>(720));
         shader->Bind();
         shader->SetMat4("projection", projection);
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::scale(model, glm::vec3(0.5f));
+        shader->SetMat4("model", model);
 //        glUniformMatrix4fv(glGetUniformLocation(shader->GetRendererID(), "projection"), 1, GL_FALSE, glm::value_ptr(projection));
 
         // FreeType
@@ -245,76 +227,11 @@ namespace Mayra
         glBindVertexArray(0);
     }
 
-//    void FreeTypeFontScene::LoadFontGlyphs()
-//    {
-//        for (int gidx = 0; gidx < face->num_glyphs; gidx++) {
-//            if (FT_Load_Glyph(face, gidx, FT_LOAD_RENDER))
-//                continue;
-//
-//            GLuint name;
-//            glGenTextures(1, &name);
-//            glBindTexture(GL_TEXTURE_2D, name);
-//            glTexImage2D(GL_TEXTURE_2D, 0, GL_RED,
-//                         slot->bitmap.width, slot->bitmap.rows,
-//                         0, GL_RED, GL_UNSIGNED_BYTE,
-//                         slot->bitmap.buffer);
-//
-//            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-//            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-//            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-//            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-//
-//            glyph[gidx].glName = name;
-//            glyph[gidx].ax = slot->advance.x >> 6;
-//            glyph[gidx].ay = slot->advance.y >> 6;
-//            glyph[gidx].bw = slot->bitmap.width;
-//            glyph[gidx].bh = slot->bitmap.rows;
-//            glyph[gidx].bl = slot->bitmap_left;
-//            glyph[gidx].bt = slot->bitmap_top;
-//        }
-//
-//        // Assign UNICODE code to glyph table
-//        uint32_t gidx;
-//        char32_t ch = FT_Get_First_Char(face, &gidx);
-//        while ( gidx != 0) {
-//            glyph[gidx].ch = ch;
-//            ch = FT_Get_Next_Char(face, ch, &gidx);
-//        }
-//
-//        string::const_iterator ch;
-//        for (ch = text.begin(); ch != text.end(); ch++) {
-//            int gidx = FT_Get_Char_Index(face, *ch);
-//
-//            float xpos = x + glyph[gidx].bl;
-//            float ypos = y - (glyph[gidx].bh - glyph[gidx].bt);
-//            float w = glyph[gidx].bw;
-//            float h = glyph[gidx].bh;
-//            float vtx[6][4] = {
-//                { xpos, ypos+h, 0.0, 0.0 },
-//                { xpos, ypos, 0.0, 1.0 },
-//                { xpos+w, ypos, 1.0, 1.0 },
-//
-//                { xpos, ypos+h, 0.0, 0.0 },
-//                { xpos+w, ypos, 1.0, 1.0 },
-//                { xpos+w, ypos+h, 1.0, 0.0 },
-//            };
-//
-//            glBindTexture(GL_TEXTURE_2D, glyph[gidx].glName);
-//            glBindBuffer(GL_ARRAY_BUFFER, vbo);
-//
-//            glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vtx), vtx);
-//
-//            glDrawArrays(GL_TRIANGLES, 0, 6);
-//            glBindBuffer(GL_ARRAY_BUFFER, 0);
-//
-//            x += glyph[gidx].ax;
-//        }
-//    }
-
     // render line of text
     // -------------------
     void FreeTypeFontScene::RenderText(Mayra::Shader* shader, std::string text, float x, float y, float scale, glm::vec3 color)
     {
+        float originalx = x;
         // activate corresponding render state
         shader->Bind();
         shader->SetVec3("textColor", color);
@@ -332,39 +249,39 @@ namespace Mayra
             if (*c == '\n')
             {
                 liney -= scaledLineSpacing / 64.0f;
-                std::cout << "scaled line spacing: " << scaledLineSpacing << std::endl;
-                std::cout << "line spacing: " << lineSpacing << std::endl;
-                std::cout << "----" << std::endl;
+                x = originalx;
+                continue;
             }
-            // get line of characters here
-            Character ch = Characters[*c];
-
-            float xpos = x + ch.Bearing.x * scale;
-            float ypos = y - (ch.Size.y - ch.Bearing.y) * scale + liney;
-
-            float w = ch.Size.x * scale;
-            float h = ch.Size.y * scale;
-            // update VBO for each character
-            float vertices[6][4] = {
-                { xpos,     ypos + h,   0.0f, 0.0f },
-                { xpos,     ypos,       0.0f, 1.0f },
-                { xpos + w, ypos,       1.0f, 1.0f },
-
-                { xpos,     ypos + h,   0.0f, 0.0f },
-                { xpos + w, ypos,       1.0f, 1.0f },
-                { xpos + w, ypos + h,   1.0f, 0.0f }
-            };
-            // render glyph texture over quad
-            glBindTexture(GL_TEXTURE_2D, ch.TextureID);
-            // update content of VBO memory
-            glBindBuffer(GL_ARRAY_BUFFER, VBO);
-            glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); // be sure to use glBufferSubData and not glBufferData
-
-            glBindBuffer(GL_ARRAY_BUFFER, 0);
-            // render quad
-            glDrawArrays(GL_TRIANGLES, 0, 6);
-            if (*c != '\n')
+            else
             {
+                // get line of characters here
+                Character ch = Characters[*c];
+
+                float xpos = x + ch.Bearing.x * scale;
+                float ypos = y - (ch.Size.y - ch.Bearing.y) * scale + liney;
+
+                float w = ch.Size.x * scale;
+                float h = ch.Size.y * scale;
+                // update VBO for each character
+                float vertices[6][4] = {
+                    { xpos,     ypos + h,   0.0f, 0.0f },
+                    { xpos,     ypos,       0.0f, 1.0f },
+                    { xpos + w, ypos,       1.0f, 1.0f },
+
+                    { xpos,     ypos + h,   0.0f, 0.0f },
+                    { xpos + w, ypos,       1.0f, 1.0f },
+                    { xpos + w, ypos + h,   1.0f, 0.0f }
+                };
+                // render glyph texture over quad
+                glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+                // update content of VBO memory
+                glBindBuffer(GL_ARRAY_BUFFER, VBO);
+                glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); // be sure to use glBufferSubData and not glBufferData
+
+                glBindBuffer(GL_ARRAY_BUFFER, 0);
+                // render quad
+                glDrawArrays(GL_TRIANGLES, 0, 6);
+
                 // now advance cursors for next glyph (note that advance is number of 1/64 pixels)
                 x += (ch.Advance >> 6) * scale; // bitshift by 6 to get value in pixels (2^6 = 64 (divide amount of 1/64th pixels by 64 to get amount of pixels))
             }
